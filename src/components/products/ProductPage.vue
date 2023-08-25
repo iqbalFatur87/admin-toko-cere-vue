d3da3c2a4e54d5f6543a2e008c66ab89cd9b3030
iqbalFatur87 <iqbalfatur18@gmail.com>
2023-08-25 11:10:27 +0700

    change mapState

--- src/components/products/ProductPage.vue
+++ src/components/products/ProductPage.vue
@@ -164,7 +164,7 @@ export default {
     },
   },
   methods: {
-    ...mapState(["products"]),
+    ...mapState(["userData", "authToken", "products"]),
     ...mapActions([
       "addProduct",
       "getShops",
